aab757dcd9930eb7723cc6e87f47b432b333fb96
xlf1024 <33392024+xlf1024@users.noreply.github.com>
2020-06-01 16:01:14 +0200

    add http authentication

--- server/main.js
+++ server/main.js
@@ -8,37 +8,81 @@ import Controller from "./Controller.js";
 import {v4 as uuid} from "uuid";
 const clients = [];
 
+const name = process.argv[2];
+const password = process.argv[3];
 const httpServer = http.createServer(handleHttp).listen(1024);
 const staticServer = new nodeStatic.Server("./",{cache: 0});
-const wsServer = new ws.Server({server: httpServer});
+const wsServer = new ws.Server({noServer: true});
 wsServer.on("connection",onWsConnect);
+httpServer.on("upgrade", onHttpUpgrade);
 
 function handleHttp(request, response){
 	console.log({url:request.url, method:request.method});
+	
+	if(!testHttpAuth(request)){
+		response.statusCode = 401;
+		response.setHeader("WWW-Authenticate", 'Basic charset="UTF-8"');
+		response.end();
+		return;
+	}
+	
 	if(request.method == "POST") return handleUpload(request, response);
 	let url = path.posix.join(request.url);
 	console.log({url});
-	if(url === "") return staticServer.serveFile("/client/dist/index.html", 200, {}, request, response);
+	if(url === "" || url === "/"){
+		response.writeHead(307, {Location:"/client/dist/index.html"});
+		response.end();
+		return;
+	}
 	try{
-		if(url.indexOf("/client/") === 0 || url.indexOf("/node_modules/") === 0) return staticServer.serveFile(url,200, {}, request, response);
-		if(url.indexOf("/upload/") === 0) return staticServer.serveFile(url, 200, {"X-Content-Type-Options": "nosniff", "Content-Type": "application/octet-stream", "X-Robots-Tag":"noindex, noarchive, nofollow"}, request, response);
+		if(url.indexOf("/client/") === 0 || url.indexOf("/node_modules/") === 0) return staticServer.serveFile(url,200, {}, request, response, staticServed);
+		if(url.indexOf("/upload/") === 0) return staticServer.serveFile(url, 200, {"X-Content-Type-Options": "nosniff", "Content-Type": "application/octet-stream", "X-Robots-Tag":"noindex, noarchive, nofollow"}, request, response, staticServed);
 	}catch(e){
 		console.error(e);
 	}
-	response.statusCode = 404;
+	response.statusCode = 403;
 	response.end();
 }
 
-function onWsConnect(socket){
-	console.log(socket);
-	clients.push(socket);
-	socket.on("message", onmessage);
-	socket.on("close", ()=>{console.log("close");clients.splice(clients.findIndex(client => client == socket),1)});
+function staticServed(err, result){
+	if(err){
+		console.log("staticServed");
+		response.writeHead(err.status, err.headers);
+		response.end();
+	}
+}
+
+function testHttpAuth(request){
+	if(!request.headers.authorization) return false;
+	
+	if(!request.headers.authorization.startsWith("Basic ")) return false;
+	
+	let [sentName,sentPassword] = atob(request.headers.authorization.split(" ")[1]).split(":");
+	
+	return sentName === name && sentPassword === password;
+}
+function atob(base64){
+	return Buffer.from(base64, "base64").toString("utf8");
+}
+
+function onHttpUpgrade(request, socket, head){
+	if(!testHttpAuth(request)){
+		socket.write('HTTP/1.1 403 Forbidden\r\n\r\n');
+		socket.destroy();
+		return;
+	}
+	wsServer.handleUpgrade(request, socket, head, (ws)=>wsServer.emit("connection",ws,request));
+}
+function onWsConnect(ws){
+	console.log(ws);
+	clients.push(ws);
+	ws.on("message", onmessage);
+	ws.on("close", ()=>{console.log("close");clients.splice(clients.findIndex(client => client == ws),1)});
 	
 	function onmessage(data){
 		console.log(data);
 		try{
-			controller.onmessage(JSON.parse(data), response => socket.send(JSON.stringify(response)));
+			controller.onmessage(JSON.parse(data), response => ws.send(JSON.stringify(response)));
 		}catch(e){
 			console.error(e);
 		}
